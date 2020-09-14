9fa7c9c1dd8b8135d8fb448fde4cff6ca52b093d
Jeen Broekstra <jeen@fastmail.com>
2020-09-14 13:34:59 +1000

    set up for maven

---
+++ src/main/java/scratchpad/App.java
@@ -0,0 +1,13 @@
+package scratchpad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
