d09df15be407b695ddec93df518ab4988b54e49f
Jeen Broekstra <jeen@fastmail.com>
2020-09-14 13:49:06 +1000

    set up for maven (#25)
    * set up for maven
    
    * add failing test
    
    * fix failing test

---
+++ src/test/java/scratchpad/AppTest.java
@@ -0,0 +1,30 @@
+package scratchpad;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    /**
+     * another test
+     */
+    @Test
+    public void testFalseApp()
+    {
+        assertFalse( false );
+    }
+}
